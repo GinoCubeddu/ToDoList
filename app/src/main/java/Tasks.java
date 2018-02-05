6e3f2d53902391a532e03aabe57c5793c16d27e1
Gino Cubeddu <cubg1_14@uni.worc.ac.uk>
2018-02-05 10:49:28 +0000

    Create tasks class to hold the retrived tasks

---
+++ app/src/main/java/Tasks.java
@@ -0,0 +1,15 @@
+/**
+ * Created by cubg1_14 on 05/02/2018.
+ */
+
+public class Tasks {
+    private String Task;
+
+    public String getTask() {
+        return Task;
+    }
+
+    public void setTask(String task) {
+        Task = task;
+    }
+}
