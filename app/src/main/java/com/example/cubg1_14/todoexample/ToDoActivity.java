43f2ee97766cb9a75e6704ac39fa3c44c6a6a313
Gino Cubeddu <cubg1_14@uni.worc.ac.uk>
2018-02-05 10:37:41 +0000

    Initial import

---
+++ app/src/main/java/com/example/cubg1_14/todoexample/ToDoActivity.java
@@ -0,0 +1,15 @@
+package com.example.cubg1_14.todoexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.backendless.Backendless;
+
+public class ToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+}
